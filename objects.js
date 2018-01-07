64007595cf10eaa48f9bb2c3bceb5822bf8e3f7d
Leo Tu <ll410119@gmail.com>
2018-01-07 02:20:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[songTitle] = artistName;
+  Object.assign(playlist, {[artistName]: songTitle});
 }
 
 function removeFromPlaylist(playlist, artistName) {
